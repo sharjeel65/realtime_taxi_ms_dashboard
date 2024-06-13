1a23a7b4a0ce52940fb75f6c458e280562bc89aa
sharjeel <sharjeel.ahmad@tuhh.de>
2024-06-13 22:27:16 +0200

    - connect with flink, elasticsearch and kibana

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,10 +1,16 @@
+from abc import ABC
+
 from pyflink.datastream import TimeCharacteristic
+from pyflink.common.typeinfo import Types
 from pyflink.datastream.functions import ProcessFunction, KeySelector, KeyedProcessFunction
 from elasticsearch import Elasticsearch
 from datetime import datetime, timedelta
+
+from pyflink.datastream.state import ValueStateDescriptor
+
 from pyflink.datastream.connectors.kafka import KafkaSource, KafkaOffsetsInitializer, FlinkKafkaConsumer
 from pyflink.datastream.stream_execution_environment import StreamExecutionEnvironment
-from pyflink.common import SimpleStringSchema, WatermarkStrategy, Types
+from pyflink.common import SimpleStringSchema, WatermarkStrategy
 
 
 class TaxiKeySelector(KeySelector):
@@ -19,22 +25,12 @@ class TaxiKeySelector(KeySelector):
 #         print(value)
 #         # No output to collector in this simple example
 
+
 class ProcessTaxiData(KeyedProcessFunction):
     def process_element(self, value, ctx):
         if not hasattr(self, 'es_client'):
             self.es_client = Elasticsearch(['http://localhost:9200'])
-            mapping = {
-                "mappings": {
-                    "properties": {
-                        "taxi_id": {"type": "keyword"},
-                        "timestamp": {"type": "date"},
-                        "location": {
-                            "type": "geo_point"
-                        }
-                    }
-                }
-            }
-            self.es_client.indices.create(index="taxi_index", body=mapping, ignore=400)
+
         fields = value.split(',')
         try:
             if len(fields) == 4:
@@ -51,23 +47,21 @@ class ProcessTaxiData(KeyedProcessFunction):
                     'taxi_id': taxi_id,
                     'timestamp': timestamp,
                     'location': {
-                        'lat': latitude,
-                        'lon': longitude
+                        'longitude': longitude,
+                        'latitude': latitude
                     }
                 }
                 # Index document into Elasticsearch
-                self.es_client.index(index='taxi_index', body=document)
+                self.es_client.index(index='flink-index', body=document)
                 print("streamed")
 
         except (ValueError, Exception) as e:
             print(f"Error processing message: {value}, Error: {e}")
 
 
-def kafka_to_elasticsearch(topic, group_id="my_consumer_group", bootstrap_servers="localhost:9094"):
+def kafka_to_elasticsearch(topic, group_id="my_consumer_group", bootstrap_servers="localhost:9092"):
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_parallelism(1)
-    # C:\Users\sharj\PycharmProjects\bd24_project_a6_b\flink - connector - kafka - 3.2
-    # .0 - 1.19.jar
     # C:\Users\sharj\PycharmProjects\bd24_project_a6_b\flink - connector - kafka - 3.2
     # .0 - 1.19.jar
     env.add_jars("file:///Users/sharj/PycharmProjects/bd24_project_a6_b/flink-connector-kafka-3.2.0-1.19.jar",
@@ -96,7 +90,7 @@ def kafka_to_elasticsearch(topic, group_id="my_consumer_group", bootstrap_server
 
     # # Define the processing logic
     stream.key_by(TaxiKeySelector()) \
-        .process(ProcessTaxiData(), output_type=Types.STRING())
+        .process(ProcessTaxiData(), output_type=None)
     # Apply KeyedProcessFunction
     # stream.key_by(lambda x: x) \
     #     .process(SimpleKeyedProcessFunction(), output_type=Types.STRING())
@@ -106,4 +100,4 @@ def kafka_to_elasticsearch(topic, group_id="my_consumer_group", bootstrap_server
 
 
 if __name__ == "__main__":
-    kafka_to_elasticsearch("taxi", "my_consumer_group", "localhost:9094")
+    kafka_to_elasticsearch("taxi_1", "my_consumer_group", "localhost:9092")
