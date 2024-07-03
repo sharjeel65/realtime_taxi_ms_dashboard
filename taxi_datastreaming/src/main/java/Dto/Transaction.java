f6f278e9dc069498e638c5bd10455b7c94608254
Mounika1199 <mounikatbuddharaju99@gmail.com>
2024-07-03 12:45:17 +0200

    Updated

---
+++ taxi_datastreaming/src/main/java/Dto/Transaction.java
@@ -0,0 +1,11 @@
+package Dto;
+
+public class Transaction {
+
+    private int TaxiID;
+    private String Timestamp;
+    private double Latitude;
+    private double Longitude;
+    private int IsStarting;
+    private int IsEnding;
+}
